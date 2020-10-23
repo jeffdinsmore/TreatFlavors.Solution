ffa8a94ba1e2744f908741eeb4d34f3a49213929
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-23 12:08:43 -0700

    added code to the flavors controller

--- TreatFlavors/Controllers/FlavorsController.cs
+++ TreatFlavors/Controllers/FlavorsController.cs
@@ -1,10 +1,116 @@
 using Microsoft.AspNetCore.Mvc;
+using TreatFlavors.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Linq;
 
 namespace TreatFlavors.Controllers
 {
   public class FlavorsController : Controller
   {
+    private readonly TreatFlavorsContext _db;
 
+    public FlavorsController(TreatFlavorsContext db)
+    {
+      _db = db;
+    }
 
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+
+    [Authorize(Roles = "Administrator")]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+        .Include(flavor => flavor.FlavorTreats)
+        .ThenInclude(join => join.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [Authorize(Roles = "Administrator")]
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      return View(thisFlavor);
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = flavor.FlavorId });
+    }
+
+    [Authorize(Roles = "Administrator")]
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpPost]
+    public ActionResult DeleteTreat(int joinId, int flavorId)
+    {
+      var flavorTreat = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(flavorTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = flavorId });
+    }
+
+    [Authorize(Roles = "Administrator")]
+    public ActionResult AddTreat(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      return View(thisFlavor);
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpPost]
+    public ActionResult AddTreat(Flavor flavor, int TreatId)
+    {
+      if (TreatId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = flavor.FlavorId });
+    }
   }
 }
