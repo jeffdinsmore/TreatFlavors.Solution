aef73a263acb1a35bddf435a6c43a783fe16e4a2
JeffDinsmore <hello@jeffdinsmore.com>
2021-08-13 21:26:17 -0700

    updated text fields to be required fields, added buttom to register on login page

--- TreatFlavors/ViewModels/LoginViewModels.cs
+++ TreatFlavors/ViewModels/LoginViewModels.cs
@@ -2,6 +2,7 @@ namespace TreatFlavors.ViewModels
 {
   public class LoginViewModel
   {
+    // [Required, Display(Name = "Email*")]
     public string Email { get; set; }
     public string Password { get; set; }
   }
